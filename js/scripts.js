88d89b3235f7ba821f8a86b286e2b221293c6b66
Aaron Paul Piotrowski <a.paul.piotrowski@gmail.com>
2013-12-03 19:18:24 -0500

    added js
    jQuery added

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	
+	$("#basecamp").hover(
+		function() { $('h1').html('<a href="#">Basecamp</a> is the project management tool you wish you had on your last project.'); },
+		function() { $('h1').html('Making collaboration productive and enjoyable for people every day.'); }
+	);
+	$("#basecamp").hover(
+		function() { $('h2').text("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's to upgrade to basecamp. Manage projects and collaborate with your team and clients the modern way."); },
+		function() { $('h2').text("Frustration-free web-based apps for collaboration, sharing information, and making decisions."); }
+	);
+
+	$("#highrise").hover(
+		function() { $('h1').html('<a href="#">Highrise</a> remembers the important things about people youd normally forget.'); },
+		function() { $('h1').html('Making collaboration productive and enjoyable for people every day.'); }
+	);
+	$("#highrise").hover(
+		function() { $('h2').text("Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise."); },
+		function() { $('h2').text("Frustration-free web-based apps for collaboration, sharing information, and making decisions."); }
+	);
+
+	$("#campfire").hover(
+		function() { $('h1').html('From near or far, <a href="#">Campfire</a> helps teams work together over the web in real-time.'); },
+		function() { $('h1').html('Making collaboration productive and enjoyable for people every day.'); }
+	);
+	$("#campfire").hover(
+		function() { $('h2').text("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in real-time private chat room. It's game changing. We couldn't run our own business without Campfire."); },
+		function() { $('h2').text("Frustration-free web-based apps for collaboration, sharing information, and making decisions."); }
+	);
+
+	$("#basecamp").hover(
+		function() { $('#arrow1').show('<img src="../images/arrow-left.png">'); },
+		function() { $('#arrow1').hide('<img src="../images/arrow-left.png">'); }
+	);
+
+
+
+
+
+
+
+
+
+
+});
